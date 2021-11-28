ee5fa11742f70f2f40df1113b48379d092103b7f
Bảo Không Ngu <BaoIT2k3>
2021-11-28 14:53:23 +0700

    Update

--- tongsonguyen.java
+++ tongsonguyen.java
@@ -2,20 +2,18 @@ import java.util.Scanner;
 public class tongsonguyen {
 
 	public static void main(String[] args) {
-		int n;
-	Scanner sc = new Scanner(System.in);	
-	System.out.print("Nhập dãy số nguyên bất kì: ");
-	n = sc.nextInt();
-	sc.close();
-	int sum =0;
-	while (n>0)
+		int n,tong=0;
+		Scanner sc = new Scanner(System.in);	
+		System.out.print("Nhập dãy số nguyên bất kì: ");
+		n = sc.nextInt();
+		sc.close();
+		while (n!=0) 
 	{
-		System.out.println("n/2 = " +n%10);
-		sum = sum + n%10;
-		n = n/10;
-		System.out.println("n/2 = "+n);		
+		tong+= n%10;
+		n/=10;
 	}
-}		
+		System.out.println("Tổng các chữ số bạn vừa nhập là: "+tong);
+	}		
 }
 
 
